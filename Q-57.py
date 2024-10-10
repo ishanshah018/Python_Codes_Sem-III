ba43aa0806a5c316e05a1900b7af2eed700dc90a
Ishan Shah <153757966+ishanshah018@users.noreply.github.com>
2024-10-10 14:52:07 +0530

    area of a trapezoid

---
+++ Q-57.py
@@ -0,0 +1,7 @@
+# Write a Python program to find the area of Trapezoid
+
+a=int(input("Enter area: "))
+b=int(input("Enter base: "))
+h=int(input("Enter height: "))
+tr=(0.5*(a+b)*h)
+print("Area Of Trapezoid is: ",tr)

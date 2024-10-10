3969c2d2ba1c49d39a9a18b2c2d974d00668f95b
Ishan Shah <153757966+ishanshah018@users.noreply.github.com>
2024-10-10 14:43:10 +0530

    to convert hours into minutes and seconds

---
+++ Q-62.py
@@ -0,0 +1,6 @@
+#write a python program to convert hours in to minutes and seconds.
+h=int(input("Enter Hours: "))
+min=h*60;
+sec=h*60*60;
+print(h,"Hours","=",min," minutes")
+print(h,"Hours","=",sec," seconds")

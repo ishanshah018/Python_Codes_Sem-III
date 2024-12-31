293c1191fb9056d36c4309b201f2ea65c9056fd5
Ishan Shah <153757966+ishanshah018@users.noreply.github.com>
2024-12-31 11:03:49 +0530

    Q-495.py

--- Q-495.py
+++ Q-495.py
@@ -1,8 +1,5 @@
 #  Write a Python program to copy the contents of a file to another file.
 
-with open("city.txt", 'r') as f1:  
-      lines = f1.readlines()
-
-with open("city_new.txt", 'w') as f2:  
-    for line in lines:
-        f2.write(line)
+with open("city.txt", 'r') as f1, open("city_new.txt", 'w') as f2:
+    for line in f1: 
+        f2.write(line)  

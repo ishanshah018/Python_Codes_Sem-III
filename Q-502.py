01ac98b51f00cb218fe011a814c0f5fee90c3d5c
Ishan Shah <153757966+ishanshah018@users.noreply.github.com>
2024-12-31 14:23:25 +0530

    Q-502.py

--- Q-502.py
+++ Q-502.py
@@ -1,6 +1,7 @@
 # Write a python program to extract a list of all four-letter words that start and end with the same letter from a given text file
 
 with open("file.txt", "r") as f:
-    for word in f.read().split():
+    data=f.read().split()
+    for word in dataf.read().split():
         if len(word) == 4 and word[0].lower() == word[-1].lower():
                 print(word)

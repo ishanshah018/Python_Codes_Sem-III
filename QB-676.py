016af828043284f4783817dfa1dcb11fe5afb173
Ishan Shah <153757966+ishanshah018@users.noreply.github.com>
2025-01-22 22:23:46 +0530

    QB-676.py

--- QB-676.py
+++ QB-676.py
@@ -3,10 +3,8 @@ class Book:
         self.pages=pages
         
     def __add__(self,other):
-        return Book(self.pages+other.pages)
+        return self.pages+other.pages
     
 d1=Book(100)
 d2=Book(200)
-d3=Book(300)
-t=d1+d2+d3
-print(t.pages)
+print(d1+d2)

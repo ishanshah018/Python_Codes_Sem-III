2ca3b7340bf43254350fa9eec2ce2fe059c7c336
Ishan Shah <153757966+ishanshah018@users.noreply.github.com>
2025-01-22 23:42:40 +0530

    Q-695.py

---
+++ Q-695.py
@@ -0,0 +1,5 @@
+# output
+[<class '__main__.Z'>, <class '__main__.K1'>, <class '__main__.C'>, <class '__main__.K3'>, <class '__main__.A'>, <class '__main__.K2'>, <class '__main__.B'>, <class '__main__.D'>, <class '__main__.E'>, <class 'object'>]
+
+# In simple words
+[Z, K1, C, K3, A, K2, B, D, E, object]
